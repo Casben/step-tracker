8e970c8fb194d681e86e7610b1be719291ca9388
casben <herbiedodge@gmail.com>
2024-05-01 14:34:38 -0500

    created xcode project

---
+++ Step Tracker/Step_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Step_TrackerApp.swift
+//  Step Tracker
+//
+//  Created by Herbert Dodge on 5/1/24.
+//
+
+import SwiftUI
+
+@main
+struct Step_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
